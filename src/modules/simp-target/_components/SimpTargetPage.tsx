2bf8946be3a49bef9f8259ab07ace556e369afc6
Danial Muntinov <danialmuntinov@MacBook-Air-Danial.local>
2025-09-22 23:39:17 +0500

    feat(simp-target): added tanstack hooks

--- src/modules/simp-target/_components/SimpTargetPage.tsx
+++ src/modules/simp-target/_components/SimpTargetPage.tsx
@@ -1,3 +1,31 @@
-export default function SimpTarget() {
-    return <div>Simp Targets Page</div>
+"use client"
+import { useSimpTargets, useCreateSimpTarget } from "@/hooks/simp-target/useSimpTarget";
+
+export default function SimpTargetPage() {
+  const { data: targets, isLoading } = useSimpTargets();
+  const createTarget = useCreateSimpTarget();
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>All Targets</h1>
+      <ul>
+        {targets?.map((t) => (
+          <li key={t.id}>{t.name} 
+          {t.description}
+          </li>
+   
+        ))}
+      </ul>
+
+      <button
+        onClick={() =>
+          createTarget.mutate({ name: "New Target", description: "Test" })
+        }
+      >
+        Add Target
+      </button>
+    </div>
+  );
 }
