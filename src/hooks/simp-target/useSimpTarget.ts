b2f8e538f403c3bc89246a454ac719ef0ee9af30
Danial Muntinov <muntinov22@mail.ru>
2025-09-22 23:39:17 +0500

    feat(simp-target): added tanstack hooks

---
+++ src/hooks/simp-target/useSimpTarget.ts
@@ -0,0 +1,61 @@
+"use client"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  getSimpTargetsApi,
+  getSimpTargetByIdApi,
+  createSimpTargetApi,
+  updateSimpTargetApi,
+  deleteSimpTargetApi,
+} from "@/api/simp-target/simpTargetApi";
+import { SimpTarget } from "@/types/simpTarget";
+
+
+export const useSimpTargets = (params?: Partial<SimpTarget>) => {
+  return useQuery({
+    queryKey: ["simpTargets", params],
+    queryFn: () => getSimpTargetsApi(params),
+  });
+};
+
+export const useSimpTarget = (id: number) => {
+  return useQuery({
+    queryKey: ["simpTarget", id],
+    queryFn: () => getSimpTargetByIdApi(id),
+    enabled: !!id, // only run if id is truthy
+  });
+};
+
+export const useCreateSimpTarget = () => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: createSimpTargetApi,
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["simpTargets"] });
+    },
+  });
+};
+
+export const useUpdateSimpTarget = () => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, data }: { id: number; data: Partial<Omit<SimpTarget, "id">> }) =>
+      updateSimpTargetApi(id, data),
+    onSuccess: (updated) => {
+      qc.invalidateQueries({ queryKey: ["simpTargets"] });
+      qc.invalidateQueries({ queryKey: ["simpTarget", updated.id] });
+    },
+  });
+};
+
+export const useDeleteSimpTarget = () => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: (id: number) => deleteSimpTargetApi(id),
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["simpTargets"] });
+    },
+  });
+};
