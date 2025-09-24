e3d49c7855496ef90b4d35c87319076537bbe681
Danial Muntinov <muntinov22@mail.ru>
2025-09-24 21:59:38 +0500

    feat(sidebar): added sidebar component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,16 +1,21 @@
 import SideBar from "@/components/ui/sidebar/sidebar";
-
-
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="flex min-h-screen ">
-      <div className="w-[200px] h-screen bg-purple-500">
-        <SideBar />
+    <SidebarProvider>
+      
+    <section className="flex min-h-screen w-full">
+      <div className="flex bg-blue-500 z-10">
+
+        <AppSidebar />
       </div>
+     
       <div className="flex-1 flex flex-col">
         <div className=" bg-red-500 h-[100px]">NavBar</div>
         <section className="flex-1 p-4">{children}</section>
       </div>
     </section>
+    </SidebarProvider>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "@/providers/queryProvider";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,11 +30,14 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="dark">
        <QueryProvider>
+       
+          
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased `}
         >
           {children}
         </body>
+  
        </QueryProvider>
     </html>
   );
