19980601a742b6de7c51c56ff5219a88d188b8b7
Danial Muntinov <muntinov22@mail.ru>
2025-10-16 15:40:02 +0200

    feat(romantic-event): added animation fade-in

--- src/modules/romantic-event/components/EventPage.tsx
+++ src/modules/romantic-event/components/EventPage.tsx
@@ -1,28 +1,36 @@
 "use client";
 import { useState } from "react";
 import { EventCard } from "./EventCard";
-import { CardSkeleton } from "@/components/ui/loadingComp/cardSkeleton"
+import { motion, AnimatePresence } from "framer-motion";
+import { CardSkeleton } from "@/components/ui/loadingComp/cardSkeleton";
 import { useGetRomanticEvent } from "@/modules/romantic-event/hooks/useRomanticEvent";
 export default function EventsPage() {
   const { data: events, isLoading } = useGetRomanticEvent();
-   if (isLoading)
-      return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          <CardSkeleton />
-          <CardSkeleton />
-          <CardSkeleton />
-        </div>
-      )
+  if (isLoading)
+    return (
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+      </div>
+    );
 
   return (
     <div>
-    
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {events?.map((event) => (
-          <EventCard key={event.id} event={event} />
-        ))}
-      </div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key="events"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.25 }}
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+        >
+          {events?.map((event) => (
+            <EventCard key={event.id} event={event} />
+          ))}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
