97518dc74b4cfa3b86325799a1134f54a3f97a05
Danial Muntinov <muntinov22@mail.ru>
2025-10-17 17:53:34 +0200

    test(middleware): unabled dashboard protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,9 +12,9 @@ export function middleware(req: NextRequest) {
 
   const token = req.cookies.get("jwt")?.value;
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 
   return NextResponse.next();
 }
